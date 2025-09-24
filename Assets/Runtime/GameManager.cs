b065a5d85e52109ef18711e90b8f8d8bc581436f
Roy de Ridder <speedruncookie@gmail.com>
2025-09-24 14:48:23 +0200

    arena and spawning functionality

--- Assets/Runtime/GameManager.cs
+++ Assets/Runtime/GameManager.cs
@@ -1,7 +1,14 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    [Header("UI Stuff")]
+    [SerializeField] private TextMeshProUGUI speedText;
+
+    [Header("Horse Details")]
+    public float horseSpeed = 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        speedText.text = string.Format("{0:#.00} kph",horseSpeed);
     }
 }
